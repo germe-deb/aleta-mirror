fcc94f196254482f11c7b3b1ad667c1241e4c93a
germedeb <FOSSgerme.deb@tuta.io>
2022-02-24 12:39:57 -0300

    now the icon pack don't have scalable icons
    this is because many icons have a transparent background in scalable, and some others don't display correctly. and links are not prepaired to they for now

--- tasks/mvscalable.sh
+++ tasks/mvscalable.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # move apps to the build folder
-cp  icons/apps/* _build/aleta/apps/scalable
+# cp  icons/apps/* _build/aleta/apps/scalable
 
 # move status
 cp icons/scalable/status/* _build/aleta/status/scalable
