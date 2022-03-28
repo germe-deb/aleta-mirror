c884b013dce864b2a64f10a1747a9244d0d9938c
germedeb <FOSSgerme.deb@tuta.io>
2022-03-28 14:30:57 -0300

    added media-playlist, media-playlist-audio, music-note and music-note-single, and added some links

---
+++ tasks/link/linkactions.sh
@@ -0,0 +1,5 @@
+ln -s media-playlist-symbolic.symbolic.png playlist-symbolic.symbolic.png
+ln -s media-playlist-symbolic.symbolic.png media-playlist-video-symbolic.symbolic.png
+ln -s media-playlist-symbolic.symbolic.png playlist-video-symbolic.symbolic.png
+ln -s media-playlist-audio-symbolic.symbolic.png playlist-audio-symbolic.symbolic.png
+ln -s media-playlist-audio-symbolic.symbolic.png media-playlist2-symbolic.symbolic.png
