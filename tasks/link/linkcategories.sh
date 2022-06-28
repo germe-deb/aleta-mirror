cc9a13f2c1e524e7c49314fb251dd9e75a0400ab
germedeb <FOSSgerme.deb@tuta.io>
2022-06-28 20:18:58 -0300

    added some links to get more compatibility with settings icons

--- tasks/link/linkcategories.sh
+++ tasks/link/linkcategories.sh
@@ -2,3 +2,5 @@
 ln -s applications-games.png applications-simulation.png
 ln -s applications.png applications-other.png
 ln -s applications-accessories.png applications-utilities.png
+ln -s preferences-other-symbolic.symbolic.png applications-system-symbolic.symbolic.png
+ln -s preferences-other-symbolic.symbolic.png preferences-symbolic.symbolic.png
